3ea63156aca6fad080d415911e73ac48498f24fb
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-08-14 17:04:35 -0400

    First hide example

--- public/js/taco.js
+++ public/js/taco.js
@@ -1,1 +1,8 @@
 console.log('Hey, this is the console');
+
+
+// Get the doge taco and hide it.
+// In JavaScript, you would need to do:
+// document.getElementById('doge-taco').style.display = "none";
+// In jQuery, you can:
+$('#doge-taco').hide();
